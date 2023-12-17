e7fb7a85ffac17247a085a852a5df09259d9c368
RappyTV <rappytv@rappytv.com>
2023-12-17 23:33:14 +0100

    Add config icons

--- core/src/main/java/com/rappytv/eshare/EShareConfig.java
+++ core/src/main/java/com/rappytv/eshare/EShareConfig.java
@@ -3,12 +3,17 @@ package com.rappytv.eshare;
 import net.labymod.api.addon.AddonConfig;
 import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting;
 import net.labymod.api.client.gui.screen.widget.widgets.input.TextFieldWidget.TextFieldSetting;
+import net.labymod.api.configuration.loader.annotation.SpriteSlot;
+import net.labymod.api.configuration.loader.annotation.SpriteTexture;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
 
+@SpriteTexture("settings")
 public class EShareConfig extends AddonConfig {
 
+    @SpriteSlot(size = 32)
     @SwitchSetting
     private final ConfigProperty<Boolean> enabled = new ConfigProperty<>(true);
+    @SpriteSlot(size = 32, x = 1)
     @TextFieldSetting
     private final ConfigProperty<String> token = new ConfigProperty<>("");
 

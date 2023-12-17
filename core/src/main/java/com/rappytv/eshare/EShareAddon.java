385e97ea84ff231ded134f13e1ca05e0f2a1dc4f
RappyTV <rappytv@rappytv.com>
2023-12-17 22:19:12 +0100

    Declare prefix component

--- core/src/main/java/com/rappytv/eshare/EShareAddon.java
+++ core/src/main/java/com/rappytv/eshare/EShareAddon.java
@@ -1,8 +1,16 @@
 package com.rappytv.eshare;
 
 import net.labymod.api.addon.LabyAddon;
+import net.labymod.api.client.component.Component;
+import net.labymod.api.client.component.format.NamedTextColor;
+import net.labymod.api.client.component.format.Style;
+import net.labymod.api.client.component.format.TextDecoration;
 
 public class EShareAddon extends LabyAddon<EShareConfig> {
+
+    public static final Component prefix = Component.empty()
+        .append(Component.text("ESHARE", Style.empty().color(NamedTextColor.WHITE).decorate(TextDecoration.BOLD)))
+        .append(Component.text(" » ", NamedTextColor.DARK_GRAY));
 
     @Override
     protected void enable() {

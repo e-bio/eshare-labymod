d58632d97a1c29290e53d07d4589dc97c07d9ebc
RappyTV <rappytv@rappytv.com>
2023-12-17 22:16:28 +0100

    Add token config option

--- core/src/main/java/com/rappytv/eshare/EShareConfig.java
+++ core/src/main/java/com/rappytv/eshare/EShareConfig.java
@@ -2,15 +2,21 @@ package com.rappytv.eshare;
 
 import net.labymod.api.addon.AddonConfig;
 import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting;
+import net.labymod.api.client.gui.screen.widget.widgets.input.TextFieldWidget.TextFieldSetting;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
 
 public class EShareConfig extends AddonConfig {
 
     @SwitchSetting
     private final ConfigProperty<Boolean> enabled = new ConfigProperty<>(true);
+    @TextFieldSetting
+    private final ConfigProperty<String> token = new ConfigProperty<>("");
 
     @Override
     public ConfigProperty<Boolean> enabled() {
         return enabled;
+    }
+    public String token() {
+        return token.get();
     }
 }
